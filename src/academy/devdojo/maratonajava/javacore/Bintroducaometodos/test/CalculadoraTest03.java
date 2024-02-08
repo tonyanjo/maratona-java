683e3e14b160a26213944e3cc7a360cec61d1ed1
tonyanjo <tonyestulano@hotmail.com>
2024-02-08 17:00:49 -0300

    Added the studies related to lesson 48 Java Virado no Jiraya

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadorTest03 {
+public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.divideDoisNumeros(0, 16);
