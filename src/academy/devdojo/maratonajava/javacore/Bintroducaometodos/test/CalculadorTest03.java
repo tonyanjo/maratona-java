bc575abc38c993f1ebca09b29ccfd4887bc1d15b
tonyanjo <tonyestulano@hotmail.com>
2024-02-08 13:11:55 -0300

    Added the studies related to lesson 46 Java Virado no Jiraya

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadorTest03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadorTest03{
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(15, 2);
+        System.out.println(result);
+    }
+}
