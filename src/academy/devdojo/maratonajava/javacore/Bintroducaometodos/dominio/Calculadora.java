7b72f7a1b93e05f2ec422ad8d9b02addac1c2d2f
tonyanjo <tonyestulano@hotmail.com>
2024-02-08 15:30:33 -0300

    Added the studies related to lesson 47 Java Virado no Jiraya

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,5 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Calculadora {
     public void somaDoisNumeros() {
         System.out.println(10 + 10);
@@ -9,14 +11,25 @@ public class Calculadora {
         System.out.println(21 - 2);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2){
+    public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
 
-    public double divideDoisNumeros(double num1, double num2){
-        if (num2 == 0 || num1 == 0){
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("Todo valor dividido por zero é igual a zero");
             return 0;
+        } else {
+            return num1 / num2;
         }
-        return num1 / num2;
+    }
+
+    public void divideDoisNumeros2(double num1, double num2) {
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("Todo valor dividido por zero é igual a zero");
+            return;
+        } else {
+            System.out.println(num1 / num2);
+        }
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadorTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadorTest03.java
@@ -2,10 +2,12 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadorTest03{
+public class CalculadorTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double result = calculadora.divideDoisNumeros(15, 2);
+        double result = calculadora.divideDoisNumeros(0, 16);
         System.out.println(result);
+        System.out.println("----------------");
+        calculadora.divideDoisNumeros2(0, 5);
     }
 }
