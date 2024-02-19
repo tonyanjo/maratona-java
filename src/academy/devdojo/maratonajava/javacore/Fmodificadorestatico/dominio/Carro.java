b4bdbeab0198da8dbe0e5e4524dff08b28220873
tonyanjo <tonyestulano@hotmail.com>
2024-02-18 22:13:09 -0300

    Study: add lesson 62 - Orientação Objetos - Métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -3,7 +3,12 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite;
+    private static double velocidadeLimite;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
 
     public void imprime() {
         System.out.println("------------");
@@ -12,9 +17,12 @@ public class Carro {
         System.out.println("Velocidade Maxima " + this.velocidadeLimite);
     }
 
-    public Carro(String nome, double velocidadeMaxima) {
-        this.nome = nome;
-        this.velocidadeMaxima = velocidadeMaxima;
+    public static void setVelocidadeLimite(double velocidadeLimite){
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+
+    public static double getVelocidadeLimite(){
+        return Carro.velocidadeLimite;
     }
 
     public String getNome() {
