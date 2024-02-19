a5f2ce386018db387925bc76aa17faeaa5886441
tonyanjo <tonyestulano@hotmail.com>
2024-02-19 00:03:18 -0300

    Study: add lesson 64 - Orientação Objetos - Associação pt 01 - Arrays com Objetos

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
+        Anime anime1 = new Anime();
         Anime anime2 = new Anime();
         Anime anime3 = new Anime();
     }
