82d73c4ecd858288f8d20857a2ccc30a095ced20
tonyanjo <tonyestulano@hotmail.com>
2024-02-19 22:32:47 -0300

    Study: add lesson 71 - Orientação Objetos - Herança pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("23056-345");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setCpf("123456779");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+        
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("13453567356");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(30000);
+        System.out.println("---------------");
+        funcionario.imprime();
+    }
+}
