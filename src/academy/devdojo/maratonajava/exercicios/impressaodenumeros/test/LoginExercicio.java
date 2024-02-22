2a27d2747922c07484984967f45bfb9f933da07c
tonyanjo <tonyestulano@hotmail.com>
2024-02-22 11:55:36 -0300

    Study: add personal exercise - teste de login - add Captcha

--- src/academy/devdojo/maratonajava/exercicios/impressaodenumeros/test/LoginExercicio.java
+++ src/academy/devdojo/maratonajava/exercicios/impressaodenumeros/test/LoginExercicio.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.exercicios.impressaodenumeros.test;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -10,8 +11,19 @@ public class LoginExercicio {
 
     private String loginColetado;
     private String senhaColetada;
-
+    private String captchaColetado;
+    private String caracteres = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private int tentativa = 0;
+    private StringBuilder captcha = new StringBuilder();
+
+    public void iniciadorCaptcha() {
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            int index = random.nextInt(this.caracteres.length());
+            this.captcha.append(caracteres.charAt(index));
+        }
+        System.out.println("Captcha: " + captcha);
+    }
 
 
     public void logar() {
@@ -24,14 +36,24 @@ public class LoginExercicio {
                 System.out.println("Usuario ou senha incorretos");
                 this.tentativa++;
                 System.out.println("Voce tem " + (3 - this.tentativa) + " tentativa(s)");
-                if (tentativa == 3){
-                    System.out.println("Usuario Bloqueado!");
+                if (tentativa == 3) {
+                    System.out.println("Resolva o Captcha para provar que voce é humano.");
+                    iniciadorCaptcha();
+                    Scanner scanner = new Scanner(in);
+                    this.captchaColetado = scanner.nextLine();
+                    if (this.captchaColetado.equals(captcha.toString())) {
+                        System.out.println("Tentativas renovadas, tente novamente.");
+                        tentativa = 0;
+                    } else {
+                        System.out.println("Usuario Bloqueado!");
+                        break;
+                    }
                 }
             }
         }
     }
 
-    public void coletarCredenciais(){
+    public void coletarCredenciais() {
         Scanner scanner = new Scanner(in);
         System.out.println("Insira o Usuario:");
         this.loginColetado = scanner.nextLine();
@@ -39,19 +61,4 @@ public class LoginExercicio {
         this.senhaColetada = scanner.nextLine();
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
