aaa409698a9b2402e93e46a20bf74703d07c6188
tonyanjo <tonyestulano@hotmail.com>
2024-03-19 12:54:38 -0300

    Study: add lesson 95 - Exceções pt 01 - Errors

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
